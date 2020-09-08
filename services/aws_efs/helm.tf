3113de43c36e78c285d42d3431d69901c96ad8e8
Ruben Sancho <17210993+xposix@users.noreply.github.com>
2020-09-08 23:11:18 +0100

    Adding Helm for EFS

--- services/aws_efs/helm.tf
+++ services/aws_efs/helm.tf
@@ -8,7 +8,12 @@ resource "helm_release" "aws_efs" {
   name = var.name
   # This Helm chart release doesn't support namespace definition
   # namespace  = kubernetes_namespace.aws_efs.metadata.0.name
-  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
-  chart      = "aws-efs-csi-driver"
-  version    = var.chart_version
+
+  # The chart in the repo is broken for the latest version.
+  # repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+  # chart   = "aws-efs-csi-driver"
+  # version = var.chart_version
+  chart = "${path.module}/helm"
+
+
 }
